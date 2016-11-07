a72328dd337025ea8018639b7592aad293a2268b
Micah Britton <micahwkb@gmail.com>
2016-11-07 13:16:34 -0500

    adjusted so that callbacks contain function calls to getHTML

--- http-functions-test.js
+++ http-functions-test.js
@@ -1,18 +1,9 @@
 ('use strict')
 
 const getHTML = require('./http-functions')
-const uppercase = require('./callbacks/print_uppercase')
-const print = require('./callbacks/printHTML')
-const lowercase = require('./callbacks/print_lowercase')
-const leet = require('./callbacks/print_1337')
 
 const requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 }
 
-
-// getHTML(requestOptions, print)
-// getHTML(requestOptions, uppercase)
-// getHTML(requestOptions, lowercase)
-getHTML(requestOptions, leet)
